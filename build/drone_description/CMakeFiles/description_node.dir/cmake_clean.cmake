d0f2b60f5d3c7473f594a70481e95501005156dc
Nobel <22ee01062@iitbbs.ac.in>
2025-11-13 22:23:23 +0530

    initial drone commit

---
+++ build/drone_description/CMakeFiles/description_node.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/description_node.dir/src/description_node.cpp.o"
+  "CMakeFiles/description_node.dir/src/description_node.cpp.o.d"
+  "description_node"
+  "description_node.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/description_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
